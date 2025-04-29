8cc866c15d07f6c8649d82d9a3340c557ed5bbc0
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2025-04-29 08:31:06 -0700

    refactor: :recycle: refactor map setup

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,21 @@
-document
-	.querySelector("arcgis-map")
-	?.addEventListener("arcgisViewReadyChange", async (ev) => {
-		const [Layer, PortalItem] = await $arcgis.import([
-			"@arcgis/core/layers/Layer.js",
-			"@arcgis/core/portal/PortalItem.js",
-		] as const);
-		const { map } = ev.target;
-		const layer = await Layer.fromPortalItem({
-			portalItem: new PortalItem({
-				id: "0b0269ba1210401dba0f1a5151c21ad3",
-			}),
-		});
-		map.add(layer);
+import type { ArcgisMapCustomEvent } from "@arcgis/map-components";
+
+const mapElement = document.querySelector("arcgis-map");
+
+if (!mapElement) {
+	throw new Error("Could not find map element");
+}
+const setupMapLayers = async (ev: ArcgisMapCustomEvent<void>) => {
+	const [Layer, PortalItem] = await $arcgis.import([
+		"@arcgis/core/layers/Layer.js",
+		"@arcgis/core/portal/PortalItem.js",
+	] as const);
+	const { map } = ev.target;
+	const milepostsLayer = await Layer.fromPortalItem({
+		portalItem: new PortalItem({
+			id: "0b0269ba1210401dba0f1a5151c21ad3",
+		}),
 	});
+	map.add(milepostsLayer);
+};
+mapElement.addEventListener("arcgisViewReadyChange", setupMapLayers);
